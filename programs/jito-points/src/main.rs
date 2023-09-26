d7b99afa20e3fb401dc31167916af5c5c4583a01
Tommy Johnson <tommyjohnson1028@gmail.com>
2023-09-25 17:34:11 -0700

    refactor to route accounts; whirlpool not playing nice with Anchor 0.28

--- programs/jito-points/src/main.rs
+++ programs/jito-points/src/main.rs
@@ -1,20 +1,14 @@
-use std::{rc::Rc, mem::align_of};
+use std::{collections::HashMap, rc::Rc};
 
+use crate::solana_utils::get_many_accounts;
 use anchor_client::{Client, Cluster};
 use anchor_lang::{prelude::Pubkey, AccountDeserialize};
+use anchor_spl::{token::Mint, token_interface::TokenAccount};
 use clap::{Arg, Command};
 use clpcpi::state::ClpVault;
 use log::*;
-use solana_account_decoder::UiAccountEncoding;
-use solana_client::{
-    nonblocking::rpc_client::RpcClient,
-    rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
-    rpc_filter::{Memcmp, RpcFilterType},
-};
-use solana_sdk::{
-    commitment_config::CommitmentConfig,
-    signature::{read_keypair_file, Keypair},
-};
+use solana_sdk::{commitment_config::CommitmentConfig, signature::Keypair};
+use whirlpool::state::{Position, Whirlpool};
 
 mod config;
 mod solana_utils;
@@ -25,6 +19,34 @@ fn start_logger() {
         .init()
         .unwrap();
 }
+
+struct VaultAccounts {
+    vault: ClpVault,
+    whirlpool: Option<Whirlpool>,
+    reserves: Vec<TokenAccount>,
+    positions: Vec<Position>,
+    lp_mint: Option<Mint>,
+}
+impl VaultAccounts {
+    fn new(vault: ClpVault) -> Self {
+        Self {
+            vault,
+            whirlpool: None,
+            reserves: vec![],
+            positions: vec![],
+            lp_mint: None,
+        }
+    }
+}
+
+enum AccountType {
+    Whirlpool,
+    Token,
+    Position,
+    Mint,
+}
+
+type KeyVaultMap = HashMap<Pubkey, (Pubkey, AccountType)>;
 
 #[tokio::main]
 async fn main() {
@@ -57,39 +79,84 @@ async fn main() {
         dummy_payer.clone(),
         CommitmentConfig::processed(),
     );
+    let program = client
+        .program(Pubkey::try_from("ArmN3Av2boBg8pkkeCK9UuCN9zSUVc2UQg1qR2sKwm8d").unwrap())
+        .unwrap();
 
     // Load all the vaults
     let vaults = load_all_vaults(&client).await;
+    let mut vault_map: HashMap<Pubkey, VaultAccounts> = HashMap::new();
     // Filter the vaults to only those with jitoSOL (and Orca owned pools)
-    let mut jito_vaults: Vec<ClpVault> = Vec::new();
-    let mut whirlpool_keys: Vec<Pubkey> = Vec::new();
-    let mut position_keys: Vec<Pubkey> = Vec::new();
-    let mut lp_mint_keys: Vec<Pubkey> = Vec::new();
-    let mut vault_reserve_accounts: Vec<Pubkey> = Vec::new();
-    for (_, vault) in vaults.into_iter(){
+    let mut key_vault_type_map: KeyVaultMap = HashMap::new();
+    let mut keys: Vec<Pubkey> = Vec::new();
+    for (vault_key, vault) in vaults.into_iter() {
         if vault.token_mint_a.eq(&jito_sol_mint) || vault.token_mint_b.eq(&jito_sol_mint) {
-            jito_vaults.push(vault);
-            whirlpool_keys.push(vault.clp);
-            lp_mint_keys.push(vault.lp_mint);
-            vault_reserve_accounts.push(vault.token_vault_a);
-            vault_reserve_accounts.push(vault.token_vault_b);
+            vault_map.insert(vault_key, VaultAccounts::new(vault));
+
+            keys.push(vault.clp);
+            key_vault_type_map.insert(vault.clp, (vault_key, AccountType::Whirlpool));
+            keys.push(vault.lp_mint);
+            key_vault_type_map.insert(vault.lp_mint, (vault_key, AccountType::Mint));
+            keys.push(vault.token_vault_a);
+            key_vault_type_map.insert(vault.token_vault_a, (vault_key, AccountType::Token));
+            keys.push(vault.token_vault_b);
+            key_vault_type_map.insert(vault.token_vault_b, (vault_key, AccountType::Token));
             for vault_position in vault.positions.iter() {
                 if vault_position.position_key.ne(&Pubkey::default()) {
-                    position_keys.push(vault_position.position_key);
+                    keys.push(vault_position.position_key);
+                    key_vault_type_map.insert(
+                        vault_position.position_key,
+                        (vault_key, AccountType::Position),
+                    );
                 }
             }
         }
-    };
+    }
 
     // TODO: Load all Whirlpools, Positions, token accoutns and LP mints for the vaults
-    
+    get_many_accounts(
+        &program.async_rpc(),
+        keys.as_ref(),
+        true,
+        |start_index, accounts| {
+            for (i, account) in accounts.iter().enumerate() {
+                let index = start_index + i;
+                let pubkey = &keys[index];
+                match account {
+                    Some(account) => {
+                        let (vault_key, account_type) = key_vault_type_map.get(pubkey).unwrap();
+                        let vault_accounts = vault_map.get_mut(vault_key).unwrap();
+                        match account_type {
+                            AccountType::Whirlpool => {
+                                // let whirlpool: Whirlpool = AccountDeserialize::try_deserialize(&mut account.data.as_ref()).unwrap();
+                            },
+                            AccountType::Token => {
+                                let token_account: TokenAccount = AccountDeserialize::try_deserialize(&mut account.data.as_ref()).unwrap();
+                                vault_accounts.reserves.push(token_account);
+                            },
+                            AccountType::Position => {
+                                // let position: Position = AccountDeserialize::try_deserialize(&mut account.data.as_ref()).unwrap();
+                            },
+                            AccountType::Mint => {
+                                let lp_mint: Mint = AccountDeserialize::try_deserialize(&mut account.data.as_ref()).unwrap();
+                                vault_accounts.lp_mint = Some(lp_mint);
+                            },
+                        }
+                    }
+                    None => {}
+                }
+            }
+        },
+    )
+    .await;
     // TODO: Caculate the TVL of each vault amount
     // TODO: Return the amount of jitoSOL denominated liquidity, owned by each LP token
 }
 
 async fn load_all_vaults(client: &anchor_client::Client<Rc<Keypair>>) -> Vec<(Pubkey, ClpVault)> {
-    let program =
-        client.program(Pubkey::try_from("ArmN3Av2boBg8pkkeCK9UuCN9zSUVc2UQg1qR2sKwm8d").unwrap()).unwrap();
+    let program = client
+        .program(Pubkey::try_from("ArmN3Av2boBg8pkkeCK9UuCN9zSUVc2UQg1qR2sKwm8d").unwrap())
+        .unwrap();
     let vault_accounts: Vec<(Pubkey, ClpVault)> = program.accounts(vec![]).await.unwrap();
     info!("{} vault_accounts", vault_accounts.len());
     return vault_accounts;
